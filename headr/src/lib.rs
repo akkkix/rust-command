6a03d308d1a9777aa24e9e62b3322d8cecff02d6
aki <x5acti@gmail.com>
2024-10-14 18:59:56 +0900

    4-2

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,8 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
+
 
 #[derive(Debug)]
 pub struct Config {
@@ -72,7 +75,41 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    let num_files = config.files.len();
+
+    for (file_num,filename) in config.files.iter().enumerate() {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut file) => {
+                if num_files > 1 {
+                    println!("{}==> {} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                        filename
+                    );
+                }
+
+                if let Some(num_bytes) = config.bytes {
+                    //let mut handle = file.take(num_bytes as u64);
+                    //let mut buffer = vec![0; num_bytes];
+                    //let bytes_read = handle.read(&mut buffer)?;
+                    //print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                    //let bytes: Result<Vec<_>, _> = file.bytes().take(num_bytes).collect();
+                    let bytes = file.bytes().take(num_bytes).collect::<Result<Vec<_>, _>>();
+                    print!("{}", String::from_utf8_lossy(&bytes?));
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{}", line);
+                        line.clear();
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
 
@@ -99,3 +136,11 @@ fn test_parse_positive_int() {
     assert!(res.is_err());
     assert_eq!(res.unwrap_err().to_string(), "0".to_string());
 }
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
